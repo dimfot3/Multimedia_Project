bc5febf028cb4496fe1b5a625912a336bcf305da
dimitrios fotiou <fotiou.dimitris3@gmail.com>
2023-01-16 03:55:30 +0200

    subband filtering comments added

--- src/encoder0.py
+++ src/encoder0.py
@@ -1,60 +1,86 @@
 import numpy as np
-import utils
+import subband_utils
 from scipy.io import wavfile
 
-def codec0(wavin, h, M, N):
-    H = utils.make_mp3_analysisfb(h, M)
-    samplerate, audiodata = wavfile.read(wavin)
-    data = np.zeros((audiodata.shape[0] + audiodata.shape[0] % (M*N), ))
-    data[:audiodata.shape[0]] = audiodata
-    
-    split_idxs_arr = utils.split_arrays_equally(np.arange(data.shape[0]), M*N)
-    Y_tot = np.zeros((len(split_idxs_arr), N, M))
-    x_frame_old = np.zeros(H.shape[0] - M, )
-    for i, idxs in enumerate(split_idxs_arr):
-        x_frame = data[idxs]
-        x_frame_buf = np.append(np.flip(x_frame), x_frame_old, axis=0)
-        x_frame_old = x_frame_buf[0:H.shape[0] - M]
-        y_frame = utils.frame_sub_analysis(x_frame_buf, H, N)
-        Y_c = utils.donothing(y_frame)
-        Y_tot[i] = Y_c
 
-    G = utils.make_mp3_synthesisfb(h, M)
-    x_hat = np.zeros((Y_tot.shape[0], N*M))
-    y_frame_old = np.zeros((G.shape[0] // M , M))
-    for fr_i in range(Y_tot.shape[0]):
-        y_frame = Y_tot[fr_i]
-        y_dc = utils.idonothing(y_frame)
-        y_frame_buff = np.append(y_frame_old, np.flip(y_dc, axis=0), axis=0)
-        y_frame_old = y_frame_buff[y_frame_buff.shape[0] - y_frame_old.shape[0]:]
-        x_hat[fr_i] = utils.frame_sub_synthesis(y_frame_buff, G)
+def codec0(wavin, h, M, N):
+    """
+    codec0 is the core of MP3 implementation. It is a 
+    simple format that only implements the subband filtering.
+    It encodes and decodes the data.
+
+    :param wavin: the path the sound file
+    :param h: the prototype filter response
+    :param M: the number of filters
+    :param N: the length of each subband
+    :return: the decoded sound and the encoded frames as KxNxM array where K 
+    the number of frames
+    """ 
+    H = subband_utils.make_mp3_analysisfb(h, M)
+    samplerate, data = wavfile.read(wavin)
+    data_len = data.shape[0]
+    data_buff = np.append(data, np.zeros((M*N, )))
+    Y_tot = np.zeros((0, N, M))
+    x_frame = np.zeros((M * (N-1) + H.shape[0]))
+    for i in range(data_len // (M*N)):
+        x_frame = np.flip(data_buff[:x_frame.shape[0]], axis=0)
+        y_frame = subband_utils.frame_sub_analysis(x_frame, H, N)
+        Y_c = subband_utils.donothing(y_frame).reshape(-1, N, M)
+        Y_tot = np.append(Y_tot, Y_c, axis=0)
+        data_buff = np.roll(data_buff, - M * N)
+
+    G = subband_utils.make_mp3_synthesisfb(h, M)
+    x_hat = np.zeros((Y_tot.shape[0]*N*M, ))
+    y_frame_buff = np.zeros((N + G.shape[0] // M, M))
+    for fr_i in range(0, Y_tot.shape[0]):
+        y_frame_buff[:N] = subband_utils.idonothing(Y_tot[fr_i])
+        x_hat[fr_i*M*N:(fr_i+1)*M*N] = np.flip(subband_utils.frame_sub_synthesis(y_frame_buff, G), axis=0)
+        y_frame_buff = np.roll(y_frame_buff, N, axis=0)
     return x_hat.reshape(-1, ), Y_tot
 
 
 def coder0(wavin, h, M, N):
-    H = utils.make_mp3_analysisfb(h, M)
+    """
+    coder0 is implementing the encoding of a sound file. This is a
+    first version that only implements the subband filtering.
+
+    :param wavin: the path the sound file
+    :param h: the prototype filter response
+    :param M: the number of filters
+    :param N: the length of each subband
+    :return: encoded frames as KxNxM array where K 
+    the number of frames
+    """ 
+    H = subband_utils.make_mp3_analysisfb(h, M)
     samplerate, data = wavfile.read(wavin)
-    split_idxs_arr = utils.split_arrays_equally(np.arange(data.shape[0]), M*N)
-    Y_tot = np.zeros((len(split_idxs_arr), N, M))
-    x_frame_old = np.zeros(H.shape[0] - M, )
-    for i, idxs in enumerate(split_idxs_arr):
-        x_frame = data[idxs]
-        x_frame_buf = np.append(np.flip(x_frame), x_frame_old, axis=0)
-        x_frame_old = x_frame_buf[0:H.shape[0] - M]
-        y_frame = utils.frame_sub_analysis(x_frame_buf, H, N)
-        Y_c = utils.donothing(y_frame)
-        Y_tot[i] = Y_c
+    data_len = data.shape[0]
+    data_buff = np.append(data, np.zeros((M*N, )))
+    Y_tot = np.zeros((0, N, M))
+    x_frame = np.zeros((M * (N-1) + H.shape[0]))
+    for i in range(data_len // (M*N)):
+        x_frame = np.flip(data_buff[:x_frame.shape[0]], axis=0)
+        y_frame = subband_utils.frame_sub_analysis(x_frame, H, N)
+        Y_c = subband_utils.donothing(y_frame).reshape(-1, N, M)
+        Y_tot = np.append(Y_tot, Y_c, axis=0)
+        data_buff = np.roll(data_buff, - M * N)
     return Y_tot
 
 def decoder0(Y_tot, h, M, N):
-    G = utils.make_mp3_synthesisfb(h, M)
-    x_hat = np.zeros((Y_tot.shape[0], N*M))
-    y_frame_old = np.zeros((G.shape[0] // M , M))
+    """
+    decoder0 is implementing the decoding of encoded sound file. This
+    is a prototype version and implements only the inverse of subband filtering.
+    :param wavin: the path the sound file
+    :param h: the prototype filter response
+    :param M: the number of filters
+    :param N: the length of each subband
+    :return: the decoded sound
+    """
+    G = subband_utils.make_mp3_synthesisfb(h, M)
+    x_hat = np.zeros((Y_tot.shape[0]*N*M, ))
+    y_frame_buff = np.zeros((N + G.shape[0] // M, M))
     for fr_i in range(Y_tot.shape[0]):
-        y_frame = Y_tot[fr_i]
-        y_dc = utils.idonothing(y_frame)
-        y_frame_buff = np.append(y_frame_old, np.flip(y_dc, axis=0), axis=0)
-        y_frame_old = y_frame_buff[y_frame_buff.shape[0] - y_frame_old.shape[0]:]
-        x_hat[fr_i] = utils.frame_sub_synthesis(y_frame_buff, G)
-    return x_hat.reshape(-1, )
+        y_frame_buff[:N] = subband_utils.idonothing(Y_tot[fr_i])
+        x_hat[fr_i*M*N:(fr_i+1)*M*N] = np.flip(subband_utils.frame_sub_synthesis(y_frame_buff, G), axis=0)
+        y_frame_buff = np.roll(y_frame_buff, N, axis=0)
+    return x_hat
 
--- subband_filtering.py
+++ subband_filtering.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import sys
 from scipy.io import wavfile
 sys.path.insert(0, 'src')
-import utils
+import subband_utils
 from encoder0 import codec0, coder0, decoder0
 
 
@@ -13,49 +13,50 @@ M = 32
 N = 36
 
 # 1.calculate the H and G
-H = utils.make_mp3_analysisfb(h_pr, M)
-G = utils.make_mp3_synthesisfb(h_pr, M)
+H = subband_utils.make_mp3_analysisfb(h_pr, M)
+G = subband_utils.make_mp3_synthesisfb(h_pr, M)
 
 # 2 and 3. ploting H in vs Hz and Bark in dB. I assume sampling rate 44100Hz
-# f, ax = plt.subplots(1, 2)
-# for i in range(M):
-#     Hi = np.abs(np.fft.fft(H[:, i]))[0 : H.shape[0] // 2]
-#     freqs = np.fft.fftfreq(H.shape[0], 1/(44100))[0 : H.shape[0] // 2]
-#     ax[0].plot(freqs, 20*np.log10(Hi))
-#     ax[0].set_ylim([-10, 1])
-#     ax[0].set_ylabel('dB', fontsize=20)
-#     ax[0].set_xlabel('Hz', fontsize=20)
-#     ax[0].set_title('h filters response Hz', fontsize=20, fontweight='bold')
-#     ax[0].tick_params(axis='x', which='both', labelsize=16)
-#     ax[0].tick_params(axis='y', which='both', labelsize=16)
-#     z = 13*np.arctan(0.00076*freqs) + 3.5*np.arctan((freqs/7500)**2)
-#     ax[1].plot(z, 20*np.log10(Hi))
-#     ax[1].set_ylim([-10, 1])
-#     ax[1].set_ylabel('dB', fontsize=20)
-#     ax[1].set_xlabel('Bark', fontsize=20)
-#     ax[1].set_title('h filter response Bark', fontsize=20, fontweight='bold')
-#     ax[1].tick_params(axis='x', which='both', labelsize=16)
-#     ax[1].tick_params(axis='y', which='both', labelsize=16)
-# plt.show()
+f, ax = plt.subplots(1, 2)
+for i in range(M):
+    Hi = np.abs(np.fft.fft(H[:, i]))[0 : H.shape[0] // 2]
+    freqs = np.fft.fftfreq(H.shape[0], 1/(44100))[0 : H.shape[0] // 2]
+    ax[0].plot(freqs, 20*np.log10(Hi))
+    ax[0].set_ylim([-10, 1])
+    ax[0].set_ylabel('dB', fontsize=20)
+    ax[0].set_xlabel('Hz', fontsize=20)
+    ax[0].set_title('h filters response Hz', fontsize=20, fontweight='bold')
+    ax[0].tick_params(axis='x', which='both', labelsize=16)
+    ax[0].tick_params(axis='y', which='both', labelsize=16)
+    z = 13*np.arctan(0.00076*freqs) + 3.5*np.arctan((freqs/7500)**2)
+    ax[1].plot(z, 20*np.log10(Hi))
+    ax[1].set_ylim([-10, 1])
+    ax[1].set_ylabel('dB', fontsize=20)
+    ax[1].set_xlabel('Bark', fontsize=20)
+    ax[1].set_title('h filter response Bark', fontsize=20, fontweight='bold')
+    ax[1].tick_params(axis='x', which='both', labelsize=16)
+    ax[1].tick_params(axis='y', which='both', labelsize=16)
+plt.show()
 
 
 music_file = './data/myfile.wav'
 xhat, Y_tot = codec0(music_file, h_pr, M, N)
 samplerate, data = wavfile.read(music_file)
 
-plt.plot(data, label='x real')
-plt.plot(xhat, label='$\hat{x}$')
-plt.legend()
+plt.plot(data.astype('int16')[0:] - xhat.astype('int16')[0:])
+plt.title('x - $\hat{x}$', fontsize=20)
+plt.ylim([data.min(), data.max()])
+plt.tick_params(axis='x', which='both', labelsize=16)
+plt.tick_params(axis='y', which='both', labelsize=16)
+plt.xlabel('time', fontsize=20)
+plt.ylabel('value', fontsize=20)
 plt.show()
 wavfile.write('test.wav', samplerate, xhat.astype('int16'))
-# # for i in range(200):
-# #     xtest = np.roll(xhat.astype('float32'), 100-i)
-# #     print(np.corrcoef(data.astype('float32'), xtest)[0, 1], 100-i)
 
-plt.plot(data.astype('float32') - xhat.astype('float32'))
+plt.plot(20*np.log10(np.abs(data.astype('int16')/(xhat.astype('int16') - data.astype('int16') + 1e-12))))
 plt.title('SNR', fontsize=20, fontweight='bold')
 plt.xlabel('samples', fontsize=20)
-plt.ylabel('SNR', fontsize=20)
+plt.ylabel('dB', fontsize=20)
 plt.xticks(fontsize=16)
 plt.yticks(fontsize=16)
 plt.show()
@@ -64,10 +65,12 @@ plt.show()
 Y_tot = coder0(music_file, h_pr, M, N)
 xhat = decoder0(Y_tot, h_pr, M, N)
 wavfile.write('test.wav', samplerate, xhat.astype('int16'))
-plt.plot(xhat, label='$\hat{x}$')
-plt.plot(data, label='x real')
-plt.legend()
-plt.show()
+
+
+# plt.plot(20*np.log10(np.abs(data.astype('int16')/(xhat.astype('int16') - data.astype('int16') + 1e-12))), label='SNR')
+# plt.show()
+
+
 
 
 
